44e5e0ab802df1fa6908540b5f70ab2387d67171
Edir P Santos <fennder@gmail.com>
2025-02-12 11:55:39 -0300

    ajuste em código script.js

--- teste/pagina_venda/script.js
+++ teste/pagina_venda/script.js
@@ -1,7 +1,8 @@
 const produtos = [
-    { nome: "Produto 1", preco: 10.00, imagem: "produto1.jpg" },
-    { nome: "Produto 2", preco: 20.00, imagem: "produto2.jpg" },
-    { nome: "Produto 3", preco: 30.00, imagem: "produto3.jpg" },
+    { nome: "Produto 1", preco: 19.00, imagem: "produto1.jpg" },
+    { nome: "Produto 2", preco: 33.00, imagem: "produto2.jpg" },
+    { nome: "Produto 3", preco: 68.00, imagem: "produto3.jpg" },
+    
 ];
 
 const produtosDiv = document.getElementById("produtos");
@@ -18,7 +19,8 @@ function exibirProdutos() {
             <img src="${produto.imagem}" alt="${produto.nome}">
             <h3>${produto.nome}</h3>
             <p>Preço: R$ ${produto.preco.toFixed(2)}</p>
-            <button onclick="adicionarAoCarrinho('${produto.nome}', ${produto.preco})">Adicionar ao Carrinho</button>
+            <button onclick="adicionarAoCarrinho('${produto.nome}', ${produto.preco})">
+            Adicionar ao Carrinho</button>
         `;
         produtosDiv.appendChild(produtoDiv);
     });
